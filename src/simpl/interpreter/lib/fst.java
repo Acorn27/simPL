9d4e6f8eed4ff4a254db95b507c647c9db87d4d1
acorn27 <maitran.mt02@gmail.com>
2023-11-04 19:21:40 -0500

    save

--- src/simpl/interpreter/lib/fst.java
+++ src/simpl/interpreter/lib/fst.java
@@ -12,28 +12,9 @@ import simpl.typing.TypeEnv;
 import simpl.typing.TypeError;
 import simpl.typing.TypeResult;
 
-public class Fst extends FunValue {
+public class fst extends FunValue {
 
-    public Fst() {
-        super(Env.empty, Symbol.symbol("x"), new Expr() {
-            Symbol x = Symbol.symbol("x");
-
-            @Override
-            public TypeResult typeCheck(TypeEnv E) {
-                // Type declaration is provided in `DefaultTypeEnv`. Type checking is done in
-                // `App`.
-                // Nothing to be done here.
-                return null;
-            }
-
-            @Override
-            public Value eval(State s) throws RuntimeError {
-                var pairVal = s.E.get(x, s);
-                if (!(pairVal instanceof PairValue)) {
-                    throw new RuntimeError("not a pair");
-                }
-                return ((PairValue) pairVal).v1;
-            }
-        });
+    public fst() {
+        super(null, null, null);
     }
 }
