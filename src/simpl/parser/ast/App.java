b1bb150934f12af77d73cb0fed365d5b9f659b37
acorn27 <maitran.mt02@gmail.com>
2023-11-09 22:41:34 -0600

    remove comments

--- src/simpl/parser/ast/App.java
+++ src/simpl/parser/ast/App.java
@@ -28,39 +28,25 @@ public class App extends BinaryExpr {
     @Override
     public TypeResult typecheck(TypeEnv E) throws TypeError {
 
-        // type check e1 and e2
         var e1Tr = this.l.typecheck(E);
         var e2Tr = this.r.typecheck(E);
 
-        // produce combined constraint to re-type check e1
         var subst = e1Tr.s.compose(e2Tr.s);
-        // produce returned type yield using by apply combined substitution on e1
         var e1Ty = subst.apply(e1Tr.t);
-        // It may not be neccesary, but keep it for now
         var e2Ty = subst.apply(e2Tr.t);
 
-        // if e1 type already an arrow type
         if (e1Ty instanceof ArrowType) {
-            // get parameter type of e1 and unified it with type of e2
             var paramTy = ((ArrowType) e1Ty).t1;
-            // produce new substitution
             subst = subst.compose(paramTy.unify(e2Ty));
-            // retrieve final type by apply new substution on body of the function
             var resTy = subst.apply(((ArrowType) e1Ty).t2);
             return TypeResult.of(subst, resTy);
-            // if e1 type is an typeVar, shape it to an arrow type
         } else if (e1Ty instanceof TypeVar) {
-            // create new return type
             var resTv = new TypeVar(true);
-            // e1Ty = e2Ty -> ResTv
             subst = subst.compose(e1Ty.unify(new ArrowType(e2Ty, resTv)));
-            // produce type yield of resTV
             var resTy = subst.apply(resTv);
             return TypeResult.of(subst, resTy);
         }
-
-        // debug
-        throw new TypeError("Lhs is not a function");
+        throw new TypeError("Lhs is not of function type");
     }
 
     @Override
