1695aaaf85692baf973781d21b5a625cca812088
acorn27 <maitran.mt02@gmail.com>
2023-11-17 10:43:01 -0600

    add features class and more comments in Mem

---
+++ src/simpl/interpreter/Features.java
@@ -0,0 +1,30 @@
+package simpl.interpreter;
+
+public class Features {
+
+    // Whether to use garbage collection
+    public static final boolean GC = true;
+
+    private static int memorySize = 100;
+
+    // public function to set memory size
+    public void setMem(int size) throws RuntimeError {
+        if (GC) {
+            memorySize = size;
+        } else {
+            throw new RuntimeError("Error: Cannot set memory size when garbage collection is disabled.");
+        }
+    }
+
+    // public function to get memory size
+    public static int getMem() throws RuntimeError {
+        if (GC) {
+            return memorySize;
+        } else {
+            throw new RuntimeError("Error: Cannot access memory size when garbage collection is disabled.");
+        }
+    }
+
+    // Whether to use lazy evaluation
+    public static final boolean LAZY = false;
+}
--- src/simpl/interpreter/Mem.java
+++ src/simpl/interpreter/Mem.java
@@ -22,9 +22,7 @@ public class Mem extends HashMap<Integer, Value> {
     public int alloc(State s) throws RuntimeError {
 
         // GC: coppy collection implementation
-        if (GC && s.p.get() >= memorySize) {
-
-            // System.out.println(String.format("Run out of mem at p = %s", s.p.get()));
+        if (Features.GC && s.p.get() >= Features.getMem()) {
 
             // PHASE 1: copy && store in continuous spacce
             // create a second halves to be used an temporary storage
@@ -48,13 +46,11 @@ public class Mem extends HashMap<Integer, Value> {
 
             // if all the memory are in used => throw error
             if (tempStorage.size() >= memorySize) {
-                throw new RuntimeError("Running out of memory");
+                throw new RuntimeError("Runtime Eror: Running out of memory");
             }
 
             // PHASE 2: coppy from temporary storage back to heap
             for (int i = 0; i < tempStorage.size(); i++) {
-                // System.out.println(String.format("After gc: %s:%s", i, tempStorage.get(i)));
-                // write approrirate value to each cell
                 write(i, tempStorage.get(i));
             }
 
@@ -62,7 +58,7 @@ public class Mem extends HashMap<Integer, Value> {
             s.p.set(tempStorage.size());
             return s.p.get();
         } else {
-            // not GC
+            // if GC is dissable
             var ptr = s.p.get();
             s.p.set(ptr + 1);
             return ptr;
