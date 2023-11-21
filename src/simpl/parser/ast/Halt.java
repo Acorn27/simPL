38b17e7a9db85a1329a9f774dfa93792f653d285
Huy <93058128+Acorn27@users.noreply.github.com>
2023-11-21 21:21:22 +0000

    save

---
+++ src/simpl/parser/ast/Halt.java
@@ -0,0 +1,33 @@
+    package simpl.parser.ast;
+
+    import simpl.interpreter.RuntimeError;
+    import simpl.interpreter.State;
+    import simpl.interpreter.Value;
+import simpl.parser.Symbol;
+import simpl.typing.TypeEnv;
+    import simpl.typing.TypeError;
+    import simpl.typing.TypeResult;
+
+    public class Halt extends Expr {
+
+        public Symbol x;
+
+        public Halt(Symbol x) {
+            this.x = x;
+        }
+
+        public String toString() {
+            return "" + x;
+        }
+
+        @Override
+        public TypeResult typecheck(TypeEnv E) throws TypeError {
+
+            return null;
+        }
+
+        @Override
+        public Value eval(State s) throws RuntimeError {
+            return null;
+        }
+    }
